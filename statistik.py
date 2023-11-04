520d74f8bd29e8ee26e6a1bad42444065552c3a8
zhavei <zhavei.vei@gmail.com>
2023-11-04 17:25:31 +0700

    latihian cleaning data

--- statistik.py
+++ statistik.py
@@ -165,3 +165,41 @@ def interpolate_quadratic(data, x):
 y = interpolate_quadratic(data, 11)
 
 print(y)
+
+#Drop
+import pandas as pd
+ 
+df = pd.read_csv("data.csv")
+ 
+Q1 = (df['TotalCharges']).quantile(0.25)
+Q3 = (df['TotalCharges']).quantile(0.75)
+IQR = Q3 - Q1
+ 
+maximum = Q3 + (1.5*IQR)
+minimum = Q1 - (1.5*IQR)
+ 
+kondisi_lower_than = df['TotalCharges'] < minimum
+kondisi_more_than = df['TotalCharges'] > maximum
+ 
+df.drop(df[kondisi_lower_than].index, inplace=True)
+df.drop(df[kondisi_more_than].index, inplace=True)
+
+#Imputation
+#df = pd.read_csv("data.csv")
+df = np.random.randn(100)
+ 
+Q1 = (df['TotalCharges']).quantile(0.25)
+Q3 = (df['TotalCharges']).quantile(0.75)
+IQR = Q3 - Q1
+ 
+maximum = Q3 + (1.5*IQR)
+minimum = Q1 - (1.5*IQR)
+ 
+kondisi_lower_than = df['TotalCharges'] < minimum
+kondisi_more_than = df['TotalCharges'] > maximum
+ 
+df.mask(cond=kondisi_more_than, maximum, axis=1, inplace=True)
+df.mask(cond=kondisi_lower_than, minimum, axis=1, inplace=True)
+
+
+
